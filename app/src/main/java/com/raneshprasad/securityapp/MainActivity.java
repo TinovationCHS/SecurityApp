9cd69aefaf3ef57fd682a3305474802f4715dbe8
raneshandroid <prasad_ranesh@yahoo.com>
2016-10-02 00:03:13 -0700

    Hello!

--- app/src/main/java/com/raneshprasad/securityapp/MainActivity.java
+++ app/src/main/java/com/raneshprasad/securityapp/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
     Button logIn;
     Button signUp;
+    //Hello
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
